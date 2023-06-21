a14a438c30872a0022c8c0d622cb19cf713d965c
joseevilasio <jose_evilasio@outlook.com>
2023-06-21 18:23:31 +0100

    estrutura inicial

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <locale.h>
+
+using namespace std;
+
+void limparTela(){
+    system("CLS");
+}
+
+int main() {
+    //Utilizar linguagem local do sistema do usuário. 
+    setlocale(LC_ALL, "");
+
+}
