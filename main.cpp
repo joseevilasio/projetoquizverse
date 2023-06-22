73b1b9a9962f946df978a6ded6e1be8432c8aa97
Tijgadixa <137155647+Tijgadixa@users.noreply.github.com>
2023-06-22 12:15:11 +0100

    Adicionei as opçoes de entrada e saida

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <string>
-#include <stdio.h>
 #include <stdlib.h>
 //#include <conio.h>
 #include <locale.h>
+#include <cctype>
 
 using namespace std;
 
@@ -12,23 +12,56 @@ void limparTela(){
     system("CLS");
 }
 
+//Tela Inicial
+char telaInicial(){
+
+    char opcao;
+
+    cout << "TriviaPlanet";
+    while(opcao != 'B') {
+
+    cout << "" << endl;
+    cout << "A - Entrar" << endl;
+    cout << "B - Sair" << endl;
+    cin >> opcao;
+    opcao = toupper(opcao);
+
+        switch (opcao) {
+            case 'A':
+            return 'A';
+            break;
+
+            case 'B' :
+            return 'B';
+            break;
+
+            default: cout << "Opção Inválida. Tente Novamente!" << endl;
+        }
+
+    }
+
+    return 'A';
+
+}
+
 
 
 int main() {
     //Utilizar linguagem local do sistema do usuário.
-    setlocale(LC_ALL, "");
+    setlocale(LC_ALL,"");
 
     //Declarar todas as variaveis aqui e comentar o que cada uma faz
     int opcao; // variavel utilizada no while de Tela Registo/Login
 
-    //Exibir Tela de Boas Vindas, resulta em um return 0 (entrar) ou 1 (sair)
+    //Exibir Tela de Boas Vindas, resulta em um return a (entrar) ou b (sair)
     //inserir funcao
+    telaInicial();
 
     //estruturar um if quando a funcao Boas Vindas tiver pronta
     while (opcao != 3) {
 
     cout << "" << endl;
-    cout << "1 Login" << endl;
+    cout << "1 Logín" << endl;
     cout << "2 Signin" << endl;
     cout << "3 Sair" << endl;
     cout << ">>>";
@@ -38,17 +71,17 @@ int main() {
             case 1:
             limparTela();
             //inserir tela de login
-            cout << "Tela Login" << endl;            
+            cout << "Tela Login" << endl;
             exit(0);
             break;
 
-            case 2: 
+            case 2:
             limparTela();
             //inserir tela de cadastro
-            cout << "Tela Cadastro" << endl;    
+            cout << "Tela Cadastro" << endl;
             break;
 
-            case 3: 
+            case 3:
             limparTela();
             //inserir tela de despedida
             cout << "Tela Despedida" << endl;
