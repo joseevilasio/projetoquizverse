585f4741ca15392d73db60a7b6966e4e233d37e0
joseevilasio <jose_evilasio@outlook.com>
2023-06-27 17:43:10 +0100

    funcoes de reset de pontos e salvar pontos do jogo

--- function_database.cpp
+++ function_database.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <sstream>
 #include <cstdio>
 #include <vector>
 #ifndef FUNCTION_DATABASE_H
@@ -59,5 +60,71 @@ int consultarLinhadeArquivo(string userEmail){
     }
     return 0;
 }
+
+string buscarLinhadeArquivo(string userEmail){
+    //Recebe email e identifica em qual linha consta os dados e retornar o valor em int
+    ifstream arquivo("assets/database.txt");  // Abre o arquivo para leitura    
+    
+    if (arquivo.is_open()){
+        string linha;
+
+        while (getline(arquivo, linha)) {
+            istringstream iss(linha);
+            string dado;
+            vector<string> dados;            
+
+            while (getline(iss, dado, ',')) {
+                dados.push_back(dado); //A cada volta no loop dentro da linha recebe o valor separado por vírgula                
+            }
+                
+            if (userEmail == dados[1]){
+                return linha;
+                break;  
+            }
+        }
+        arquivo.close();
+    } else {
+        cout << "Falha ao abrir o arquivo." << endl;
+    }
+    return "";
+}
+
+void modificarPontos(string userEmail, int pontosUser) {
+
+    string nomeArquivo = "database.txt";
+
+    ifstream arquivoEntrada(nomeArquivo);
+    ofstream arquivoTemporario("temp.txt");
+
+    string linha;
+    int numeroLinha = 1;
+    int linhaParaEliminar = consultarLinhadeArquivo(userEmail);
+
+    while (getline(arquivoEntrada, linha)) {
+        if (numeroLinha != linhaParaEliminar) {
+            arquivoTemporario << linha << endl;
+        }
+        numeroLinha++;
+    }
+
+    istringstream iss(buscarLinhadeArquivo(userEmail));
+    string dado;
+    vector<string> dados; 
+
+    while (getline(iss, dado, ',')) {
+                dados.push_back(dado); //A cada volta no loop dentro da linha recebe o valor separado por vírgula                
+            }
+    
+
+    arquivoTemporario << dados[0] << "," << dados[1] << "," << dados[2] << "," << dados[3] << "," << dados[4] << "," << pontosUser << endl;
+
+    arquivoEntrada.close();
+    arquivoTemporario.close();
+
+    remove(nomeArquivo.c_str());
+    rename("temp.txt", nomeArquivo.c_str());
+}
+
+
 
 #endif
