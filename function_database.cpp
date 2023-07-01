fe616ece9f1e68b3631405a6c91cde9796edd307
joseevilasio <jose_evilasio@outlook.com>
2023-07-01 18:32:29 +0100

    refatoramento utilizando struct para CRUD

--- function_database.cpp
+++ function_database.cpp
@@ -12,6 +12,18 @@
 using namespace std;
 namespace fs = filesystem;
 
+struct Usuario {
+    // struct para armazenar os dados do usuário
+    string nomeCompleto, email, password, pergunta, resposta;
+    int pontos;
+    };
+
+struct Perguntas {
+    // struct para armazenar os dados de perguntas
+        string tema, pergunta, respostaA, respostaB, respostaC, respostaD;
+        char respostaCorreta;
+    };
+
 string path(string nomeArquivo) {
     //Recebe um nome de arquivo que está na pasta assets    
 
@@ -25,66 +37,15 @@ string path(string nomeArquivo) {
     #endif
 }
 
-void eliminarLinhaArquivo(const string& nomeArquivo, int linhaParaEliminar) {
+vector<Perguntas> databasePerguntas (string nomeArquivo) {
+    // Recebe path e retorna um vector com as structs
+    Perguntas questao; // instancia da struct
+    vector<Perguntas> questoes; //Recebe as structs
 
-    string _nomeArquivo = "assets/database.txt";
-    ifstream arquivoEntrada(nomeArquivo);
-    ofstream arquivoTemporario("temp.txt");
-
-    string linha;
-    int numeroLinha = 1;
-
-    while (getline(arquivoEntrada, linha)) {
-        if (numeroLinha != linhaParaEliminar) {
-            arquivoTemporario << linha << endl;
-        }
-        numeroLinha++;
-    }
-
-    arquivoEntrada.close();
-    arquivoTemporario.close();
-
-    remove(_nomeArquivo.c_str());
-    rename("temp.txt", _nomeArquivo.c_str());
-}
-
-int consultarLinhadeArquivo(string userEmail){
-    //Recebe email e identifica em qual linha consta os dados e retornar o valor em int
-    ifstream arquivo(path("database.txt"));  // Abre o arquivo para leitura
-    int contarLinha = 0;
+    ifstream arquivo(path(nomeArquivo)); // recebe de acordo com tema e dificuldade escolhida o path do arquivo de perguntas    
     
     if (arquivo.is_open()){
-        string linha;
-
-        while (getline(arquivo, linha)) {
-            istringstream iss(linha);
-            string dado;
-            vector<string> dados;
-
-            contarLinha += 1;
-
-            while (getline(iss, dado, ',')) {
-                dados.push_back(dado); //A cada volta no loop dentro da linha recebe o valor separado por vírgula                
-            }
-                
-            if (userEmail == dados[1]){
-                return contarLinha;
-                break;  
-            }
-        }
-        arquivo.close();
-    } else {
-        cout << "Falha ao abrir o arquivo." << endl;
-    }
-    return 0;
-}
-
-string buscarLinhadeArquivo(string userEmail){
-    //Recebe email e identifica em qual linha consta os dados e retornar o valor em int
-    ifstream arquivo(path("database.txt"));  // Abre o arquivo para leitura    
-    
-    if (arquivo.is_open()){
-        string linha;
+        string linha;        
 
         while (getline(arquivo, linha)) {
             istringstream iss(linha);
@@ -94,54 +55,127 @@ string buscarLinhadeArquivo(string userEmail){
             while (getline(iss, dado, ',')) {
                 dados.push_back(dado); //A cada volta no loop dentro da linha recebe o valor separado por vírgula                
             }
-                
-            if (userEmail == dados[1]){
-                return linha;
-                break;  
-            }
+
+            questao.tema = dados[0];
+            questao.pergunta = dados[1];
+            questao.respostaA = dados[2];
+            questao.respostaB = dados[3];
+            questao.respostaC = dados[4];
+            questao.respostaD = dados[5];
+            questao.respostaCorreta = dados[6][0];
+
+            questoes.push_back(questao);
         }
+
         arquivo.close();
+
     } else {
         cout << "Falha ao abrir o arquivo." << endl;
     }
-    return "";
+
+    return questoes;
 }
 
-void modificarPontos(string userEmail, int pontosUser) {
+vector<Usuario> databaseUsuarios () {
+    // Recebe path e retorna um vector com as structs
+    Usuario usuario; // instancia da struct
+    vector<Usuario> usuarios; //Recebe as structs
+
+    ifstream arquivo(path("database.txt")); // recebe de acordo com tema e dificuldade escolhida o path do arquivo de perguntas    
     
-    string _nomeArquivo = "assets/database.txt";
-    ifstream arquivoEntrada(path("database.txt"));
-    ofstream arquivoTemporario("temp.txt");
+    if (arquivo.is_open()){
+        string linha;        
 
-    string linha;
-    int numeroLinha = 1;
-    int linhaParaEliminar = consultarLinhadeArquivo(userEmail);
+        while (getline(arquivo, linha)) {
+            istringstream iss(linha);
+            string dado;
+            vector<string> dados;            
 
-    while (getline(arquivoEntrada, linha)) {
-        if (numeroLinha != linhaParaEliminar) {
-            arquivoTemporario << linha << endl;
-        }
-        numeroLinha++;
-    }
-
-    istringstream iss(buscarLinhadeArquivo(userEmail));
-    string dado;
-    vector<string> dados; 
-
-    while (getline(iss, dado, ',')) {
+            while (getline(iss, dado, ',')) {
                 dados.push_back(dado); //A cada volta no loop dentro da linha recebe o valor separado por vírgula                
             }
+
+            usuario.nomeCompleto = dados[0];
+            usuario.email = dados[1];
+            usuario.password = dados[2];
+            usuario.pergunta = dados[3];
+            usuario.resposta = dados[4];
+            usuario.pontos = stoi(dados[5]);
+
+            usuarios.push_back(usuario);
+        }
+
+        arquivo.close();
+
+    } else {
+        cout << "Falha ao abrir o arquivo." << endl;
+    }
+
+    return usuarios;
+}
+
+void eliminarDados(string userEmail) {
+    // Recebe argumentos para eliminar linha do arquivo
+
+    string _nomeArquivo = "assets/database.txt";    
+    ofstream arquivoTemporario("temp.txt");    
+
+    for(const auto& usuario : databaseUsuarios()) {          
+        if (userEmail != usuario.email){
+            
+            arquivoTemporario << usuario.nomeCompleto << "," << usuario.email << "," << usuario.password;
+            arquivoTemporario << "," << usuario.pergunta << "," << usuario.resposta <<  "," << usuario.pontos << endl;            
+        }
+    }
     
-
-    arquivoTemporario << dados[0] << "," << dados[1] << "," << dados[2] << "," << dados[3] << "," << dados[4] << "," << pontosUser << endl;
-
-    arquivoEntrada.close();
     arquivoTemporario.close();
 
     remove(_nomeArquivo.c_str());
     rename("temp.txt", _nomeArquivo.c_str());
 }
 
+int consultarLinhadeArquivo(string userEmail){
+    //Recebe email e identifica em qual linha consta os dados e retornar o valor em int
+    Usuario usuario; // instancia da struct    
+    int contarLinha = 0;
+   
+    for(const auto& usuario : databaseUsuarios()) {          
+        if (userEmail == usuario.email){            
+            return contarLinha;
+            break;
+        }
+        contarLinha ++;
+    }  
+    return 0;
+}
 
+void modificarPontos(string userEmail, int pontosUser) {
+    
+    string _nomeArquivo = "assets/database.txt";    
+    ofstream arquivoTemporario("temp.txt");    
+
+    for(const auto& usuario : databaseUsuarios()) {          
+        if (userEmail != usuario.email){
+            
+            arquivoTemporario << usuario.nomeCompleto << "," << usuario.email << "," << usuario.password;
+            arquivoTemporario << "," << usuario.pergunta << "," << usuario.resposta <<  "," << usuario.pontos << endl;
+            
+        }
+    }
+
+    for(const auto& usuario : databaseUsuarios()) {          
+        if (userEmail == usuario.email){
+            
+            arquivoTemporario << usuario.nomeCompleto << "," << usuario.email << "," << usuario.password;
+            arquivoTemporario << "," << usuario.pergunta << "," << usuario.resposta <<  "," << pontosUser << endl;
+            break;
+        }
+    }
+    
+    arquivoTemporario.close();
+
+    remove(_nomeArquivo.c_str());
+    rename("temp.txt", _nomeArquivo.c_str());
+}
 
 #endif
